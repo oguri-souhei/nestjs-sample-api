beb2ddae5f5a9cfde2366e5d4299e4503e65f4ec
souhei.oguri <souhei.oguri@gmail.com>
2023-01-01 14:18:35 +0900

    ✨ use db to save items.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+    }),
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemsDto } from './dto/create-items.dto';
 import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
@@ -7,20 +7,20 @@ import { v4 as uuid } from 'uuid';
 @Injectable()
 export class ItemsService {
   private items = [
-    // {
-    //   id: 'b387bc6b-694b-4790-a6da-64bd622d9a8c',
-    //   name: 'foo',
-    //   price: 1000,
-    //   description: 'this is foo.',
-    //   status: ItemStatus.ON_SALE,
-    // },
-    // {
-    //   id: 'b387bc6b-694b-4790-a6da-64bd622d9a8d',
-    //   name: 'bar',
-    //   price: 2000,
-    //   description: 'this is bar.',
-    //   status: ItemStatus.ON_SALE,
-    // },
+    {
+      id: 'b387bc6b-694b-4790-a6da-64bd622d9a8c',
+      name: 'foo',
+      price: 1000,
+      description: 'this is foo.',
+      status: ItemStatus.ON_SALE,
+    },
+    {
+      id: 'b387bc6b-694b-4790-a6da-64bd622d9a8d',
+      name: 'bar',
+      price: 2000,
+      description: 'this is bar.',
+      status: ItemStatus.ON_SALE,
+    },
   ];
 
   findAll(): Item[] {
@@ -28,7 +28,11 @@ export class ItemsService {
   }
 
   findById(id: string): Item {
-    return this.items.find((item) => item.id === id);
+    const item = this.items.find((item) => item.id === id);
+    if (!item) {
+      throw new NotFoundException();
+    }
+    return item;
   }
 
   create(itemDto: CreateItemsDto) {
